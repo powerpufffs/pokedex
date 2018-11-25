b7f2b6f00118214ec2d3832400b3bfc0f0b4adb6
Z <asterixandisaac@gmail.com>
2018-11-24 17:26:18 -0700

    first commit

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -1,10 +1,18 @@
+//Pokedex
 //
-//  ViewController.swift
-//  pokedex
+//Using the a public Pokemon API (https://pokeapi.co), create an app that displays a list of all Pokemon. Upon selecting a Pokemon, display a detail page with information about the selected Pokemon. Here are some guidelines to follow:
 //
-//  Created by Z Tai on 11/24/18.
-//  Copyright © 2018 Z Tai. All rights reserved.
+//Write all code in Swift.
+//The initial view controller should simply display the names of the Pokemon sorted by their ID.
+//The detail view controller should display at least the Pokemon's name, weight, and height. Any other information (image, description, types, etc) is not mandatory.
+//Demonstrate clean code & good coding practices. (e.g. avoid magic variables, follow MVC principles, avoid force unwrapping, add comments, etc.)
+//Use GitHub to store and version your code. Learn how to use git from the command line if you don’t already know how.
 //
+//Use the Pokeapi V2 documentation to help you understand the attributes returned by the service and how to access the ones you’ll need.  The following request will get you the initial list of Pokemon: http://pokeapi.co/api/v2/pokemon/.  If you have any questions about the project specs, feel free to reach out to me.
+//
+//Of course, I’m not expecting a master piece. I actually don’t even care whether or not it works in the end. I just want to see your way of tackling the problem and to see how far you can get. Please put your code into a public GitHub repository so that I can see it when you’re done. When you’re finished, email me back with a link to your GitHub repository so that I can review it and potentially set up an interview.
+//
+//I will be sending this same coding challenge to all other applicants. All applications will be due prior to next Friday, November 30th. To be considered for an interview, please respond with your challenge before then.
 
 import UIKit
 
